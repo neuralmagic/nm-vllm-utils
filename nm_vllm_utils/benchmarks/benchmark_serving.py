0f9bdab29cd498940ffbd0f81dffbc71ddc09785
Oleh <oleh.viniarchy@techery.io>
2024-05-21 13:30:47 +0200

    - Added some general refactoring

--- nm_vllm_utils/benchmarks/benchmark_serving.py
+++ nm_vllm_utils/benchmarks/benchmark_serving.py
@@ -356,7 +356,7 @@ async def benchmark(
 
     for row in request_details:
         for item, width in zip(row, column_widths):
-            print(item.ljust(width + 4), end='')
+            print(item.ljust(width + 30), end='')
         print()
 
     print(
